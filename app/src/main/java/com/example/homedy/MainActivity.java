50c4f3982385444c4761433c25a16ec4371427d6
IsaacNolasco <usis033721@ugb.edu.sv>
2023-04-25 12:05:03 -0600

    tercera parte111

--- app/src/main/java/com/example/homedy/MainActivity.java
+++ app/src/main/java/com/example/homedy/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.homedy;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SearchView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_add, btn_add_fragment, btn_exit;
+    SearchView search_view;
+    DownloadManager.Query query;
+
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
