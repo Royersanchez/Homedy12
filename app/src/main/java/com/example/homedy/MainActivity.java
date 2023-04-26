7b70416f56b22fd5b849b67cda94f34c5bc2daf0
Cristian Isaac Nolasco Martinez <usis033721@ugb.edu.sv>
2023-04-26 15:34:03 -0600

    ¨completo y no  terminado

--- app/src/main/java/com/example/homedy/MainActivity.java
+++ app/src/main/java/com/example/homedy/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.homedy;
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.app.DownloadManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.SearchView;
 
@@ -19,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent ventana = new Intent(getApplicationContext(), politica.class);
+        startActivity(ventana);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
 
     }
 
